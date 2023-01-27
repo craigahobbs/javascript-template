99689ad904bd9e919ba18dae81e8c8fe25142e03
craigahobbs@gmail.com <Craig A. Hobbs>
2023-01-27 13:30:11 -0800

    fix application tests

--- template/test/testApp.js
+++ template/test/testApp.js
@@ -10,23 +10,6 @@ import {{'{'}}{{packageClass}}} from '../lib/app.js';
 import test from 'ava';
 
 
-// Helper class to mock window.location instances
-class MockLocation {
-    constructor(href, hash) {
-        this._href = [href];
-        this.hash = hash;
-    }
-
-    get href() {
-        return this._href[this._href.length - 1];
-    }
-
-    set href(href) {
-        this._href.push(href);
-    }
-}
-
-
 test('{{packageClass}}, constructor', (t) => {
     const {window} = new JSDOM();
     const app = new {{packageClass}}(window);
@@ -37,7 +20,6 @@ test('{{packageClass}}, constructor', (t) => {
 
 test('{{packageClass}}.run', (t) => {
     const {window} = new JSDOM();
-
     const app = new {{packageClass}}(window);
     app.run();
     t.deepEqual(app.params, {});
@@ -48,62 +30,44 @@ test('{{packageClass}}.run', (t) => {
 
 test('{{packageClass}}.render', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/', '#')});
-    t.is(window.location.href, 'http://localhost/');
-
     const app = new {{packageClass}}(window);
     app.render();
     t.deepEqual(app.params, {});
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/']);
 
     // Re-render with same args and verify render short-circuit
     app.render();
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/']);
 });
 
 
 test('{{packageClass}}.render, hash ID', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/#hash-id', '#hash-id')});
-    t.is(window.location.href, 'http://localhost/#hash-id');
-
+    window.location.hash = '#hash-id';
     const app = new {{packageClass}}(window);
     app.render();
     t.deepEqual(app.params, {});
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/#hash-id', '#hash-id']);
 
     // Re-render with same args and verify render short-circuit
     app.render();
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/#hash-id', '#hash-id']);
 });
 
 
 test('{{packageClass}}.render, hash parameter error', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/#foo=bar', '#foo=bar')});
-    t.is(window.location.href, 'http://localhost/#foo=bar');
-
+    window.location.hash = '#foo=bar';
     const app = new {{packageClass}}(window);
     app.render();
     t.is(app.window, window);
     t.is(app.params, null);
     t.is(window.document.title, 'MyPackage');
     t.is(window.document.body.innerHTML, "<p>Error: Unknown member 'foo'</p>");
-    t.deepEqual(window.location._href, ['http://localhost/#foo=bar']);
 });
 
 
--- test-expected/noapp-0/test/testApp.js
+++ test-expected/noapp-0/test/testApp.js
@@ -8,23 +8,6 @@ import {MyPackage} from '../lib/app.js';
 import test from 'ava';
 
 
-// Helper class to mock window.location instances
-class MockLocation {
-    constructor(href, hash) {
-        this._href = [href];
-        this.hash = hash;
-    }
-
-    get href() {
-        return this._href[this._href.length - 1];
-    }
-
-    set href(href) {
-        this._href.push(href);
-    }
-}
-
-
 test('MyPackage, constructor', (t) => {
     const {window} = new JSDOM();
     const app = new MyPackage(window);
@@ -35,7 +18,6 @@ test('MyPackage, constructor', (t) => {
 
 test('MyPackage.run', (t) => {
     const {window} = new JSDOM();
-
     const app = new MyPackage(window);
     app.run();
     t.deepEqual(app.params, {});
@@ -46,62 +28,44 @@ test('MyPackage.run', (t) => {
 
 test('MyPackage.render', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/', '#')});
-    t.is(window.location.href, 'http://localhost/');
-
     const app = new MyPackage(window);
     app.render();
     t.deepEqual(app.params, {});
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/']);
 
     // Re-render with same args and verify render short-circuit
     app.render();
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/']);
 });
 
 
 test('MyPackage.render, hash ID', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/#hash-id', '#hash-id')});
-    t.is(window.location.href, 'http://localhost/#hash-id');
-
+    window.location.hash = '#hash-id';
     const app = new MyPackage(window);
     app.render();
     t.deepEqual(app.params, {});
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/#hash-id', '#hash-id']);
 
     // Re-render with same args and verify render short-circuit
     app.render();
     t.is(window.document.title, 'Hello');
     t.is(window.document.body.innerHTML, '<p>Hello</p>');
-    t.deepEqual(window.location._href, ['http://localhost/#hash-id', '#hash-id']);
 });
 
 
 test('MyPackage.render, hash parameter error', (t) => {
     const {window} = new JSDOM();
-
-    delete window.location;
-    Object.assign(window, {'location': new MockLocation('http://localhost/#foo=bar', '#foo=bar')});
-    t.is(window.location.href, 'http://localhost/#foo=bar');
-
+    window.location.hash = '#foo=bar';
     const app = new MyPackage(window);
     app.render();
     t.is(app.window, window);
     t.is(app.params, null);
     t.is(window.document.title, 'MyPackage');
     t.is(window.document.body.innerHTML, "<p>Error: Unknown member 'foo'</p>");
-    t.deepEqual(window.location._href, ['http://localhost/#foo=bar']);
 });
 
 
